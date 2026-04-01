96ef78893c6ba9616d9bcbcb1f04269c8c6335ab
Kresnananta <akresnaananta@gmail.com>
2026-04-01 23:14:09 +0700

    feat: added mqtt protocol, requirements.txt

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -1,16 +1,31 @@
 import serial
 import time
 import json
+import paho.mqtt.client as mqtt
+import os
+from dotenv import load_dotenv
 
-PORT = 'COM7'
-BAUDRATE = 115200
+load_dotenv()
+
+PORT = os.getenv('SERIAL_PORT')
+BAUDRATE = os.getenv('BAUDRATE')
+
+MQTT_BROKER = os.getenv('MQTT_BROKER')
+MQTT_PORT = int(os.getenv('MQTT_PORT'))
+MQTT_TOPIC = os.getenv('MQTT_TOPIC')
+
+mqtt_client = mqtt.Client()
 
 try:
-    ser = serial.Serial(PORT, BAUDRATE, timeout=1)
+    print(f"Menghubungkan ke MQTT Broker {MQTT_BROKER}...")
+    mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
+    mqtt_client.loop_start() # Jalankan MQTT di background
+    print("Berhasil terhubung ke MQTT Server!\n")
 
+
+    ser = serial.Serial(PORT, BAUDRATE, timeout=1)
     ser.setDTR(False)
     ser.setRTS(False)
-
     ser.reset_input_buffer()
 
     print(f"--- Terhubung ke ESP32 di {PORT} ---")
@@ -24,6 +39,10 @@ try:
                     data = json.loads(line)
                     print(f'Data dari ESP32: {data}')
 
+                    payload = json.dumps(data)
+                    mqtt_client.publish(MQTT_TOPIC, payload)
+                    print(f'   [MQTT] Terkirim -> {MQTT_TOPIC}')
+
                 except json.JSONDecodeError:
                     print(f'Bukan JSON: {line}')
                 
@@ -34,3 +53,5 @@ except Exception as e:
 
 except KeyboardInterrupt:
     print('\nProgram dihentikan user')
+    mqtt_client.loop_stop()
+    mqtt_client.disconnect()
